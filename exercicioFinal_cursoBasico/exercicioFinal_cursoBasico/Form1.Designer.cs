bfe471a7c9bd718a96769c8252170370615bc72c
egili <e.gili.mfp@gmail.com>
2021-03-21 10:17:44 -0300

    exercicio final do curso basico

---
+++ exercicioFinal_cursoBasico/exercicioFinal_cursoBasico/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace exercicioFinal_cursoBasico
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_total = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_codigo = new System.Windows.Forms.TextBox();
+            this.lst_caixa = new System.Windows.Forms.ListBox();
+            this.pic_imagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_imagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(105, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Cantina";
+            // 
+            // lbl_total
+            // 
+            this.lbl_total.AutoSize = true;
+            this.lbl_total.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_total.Location = new System.Drawing.Point(28, 240);
+            this.lbl_total.Name = "lbl_total";
+            this.lbl_total.Size = new System.Drawing.Size(0, 28);
+            this.lbl_total.TabIndex = 1;
+            this.lbl_total.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(42, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Código:";
+            // 
+            // txt_codigo
+            // 
+            this.txt_codigo.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_codigo.Location = new System.Drawing.Point(28, 94);
+            this.txt_codigo.Name = "txt_codigo";
+            this.txt_codigo.Size = new System.Drawing.Size(100, 31);
+            this.txt_codigo.TabIndex = 3;
+            this.txt_codigo.TextChanged += new System.EventHandler(this.txt_codigo_TextChanged);
+            // 
+            // lst_caixa
+            // 
+            this.lst_caixa.FormattingEnabled = true;
+            this.lst_caixa.ItemHeight = 15;
+            this.lst_caixa.Location = new System.Drawing.Point(135, 68);
+            this.lst_caixa.Name = "lst_caixa";
+            this.lst_caixa.Size = new System.Drawing.Size(185, 169);
+            this.lst_caixa.TabIndex = 4;
+            // 
+            // pic_imagem
+            // 
+            this.pic_imagem.Location = new System.Drawing.Point(28, 131);
+            this.pic_imagem.Name = "pic_imagem";
+            this.pic_imagem.Size = new System.Drawing.Size(100, 106);
+            this.pic_imagem.TabIndex = 5;
+            this.pic_imagem.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(455, 337);
+            this.ControlBox = false;
+            this.Controls.Add(this.pic_imagem);
+            this.Controls.Add(this.lst_caixa);
+            this.Controls.Add(this.txt_codigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_total);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Exercício Final";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_imagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_total;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_codigo;
+        private System.Windows.Forms.ListBox lst_caixa;
+        private System.Windows.Forms.PictureBox pic_imagem;
+    }
+}
+
---
+++ exercicioFinal_cursoBasico/exercicioFinal_cursoBasico/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicioFinal_cursoBasico
+{
+    public partial class Form1 : Form
+
+    {
+        string[] produtos = new string[6];
+        string[] codigo = new string[6];
+        double[] valor = new double[6];
+        double soma;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_codigo_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_codigo.Text.Length == 3)
+            {
+                int indice = 0;
+                for (int prod = 1; prod < codigo.Length; prod++) 
+                {
+                    if (txt_codigo.Text == codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+                if (indice == 0)
+                {
+                    MessageBox.Show("Produto não encontrado");
+                }
+                else
+                {
+                    lst_caixa.Items.Add(txt_codigo.Text + " - - " + produtos[indice] + " -- R$ " + valor[indice]);
+                    //adiciona no listbox, concatenando cod/num/valor
+
+                    soma = soma + valor[indice];
+                    lbl_total.Text = ("Valor Total R$ " + soma);
+
+                    pic_imagem.ImageLocation = "c:/imagens/" + codigo[indice] +".jpg";
+                    txt_codigo.Text = "";
+                    txt_codigo.Focus();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarArray();
+            soma = 0;
+        }
+
+        private void carregarArray()
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+
+            produtos[1] = "Assado";
+            produtos[2] = "Frito";
+            produtos[3] = "Refrigerante";
+            produtos[4] = "Suco";
+            produtos[5] = "Água de Coco";
+
+            valor[1] = 6.00;
+            valor[2] = 5.50;
+            valor[3] = 4.50;
+            valor[4] = 9.50;
+            valor[5] = 5.50;
+        }
+    }
+}
